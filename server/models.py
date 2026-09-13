033fac33c3f4f838c0706d509390bc18dfab3035
Zakaria Abdi <zakariaabdisheikh004@gmail.com>
2026-09-13 14:37:04 +0300

    Add review model and serialization support

--- server/models.py
+++ server/models.py
@@ -17,6 +17,9 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    reviews = db.relationship('Review', back_populates='customer')
+    items = association_proxy('reviews', 'item')
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
@@ -28,5 +31,44 @@ class Item(db.Model):
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship('Review', back_populates='item')
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}>'
+
+
+class CustomerSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+    reviews = fields.List(fields.Nested(lambda: ReviewSchema(exclude=('customer', 'item'))))
+
+
+class ItemSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+    price = fields.Float()
+    reviews = fields.List(fields.Nested(lambda: ReviewSchema(exclude=('customer', 'item'))))
+
+
+class ReviewSchema(Schema):
+    id = fields.Int(dump_only=True)
+    comment = fields.Str()
+    customer = fields.Nested(lambda: CustomerSchema(exclude=('reviews',)), dump_only=True)
+    item = fields.Nested(lambda: ItemSchema(exclude=('reviews',)), dump_only=True)
+    customer_id = fields.Int(load_only=True)
+    item_id = fields.Int(load_only=True)
